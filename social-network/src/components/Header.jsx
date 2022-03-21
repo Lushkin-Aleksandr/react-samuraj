10eb90340b1a05aef1454c3290c4cd691cc74359
sanyalu <sanyalu97@gmail.com>
2022-03-21 20:34:01 +0300

    start

---
+++ social-network/src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <header className='header'>
+            <img
+                src="https://www.freepnglogos.com/uploads/logo-website-png/logo-website-file-globe-icon-svg-wikimedia-commons-21.png"
+                alt=""/>
+        </header>
+    )
+}
+
+export default Header;
